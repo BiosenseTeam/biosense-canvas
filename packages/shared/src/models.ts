aa20c509c5c2b728731eef54f8a0c1d90e339d43
VtSilveira <vitor.silveiravtss@gmail.com>
2025-04-10 10:12:10 -0300

    enable dev only models

--- packages/shared/src/models.ts
+++ packages/shared/src/models.ts
@@ -425,12 +425,12 @@ const GEMINI_MODELS: ModelConfigurationParams[] = [
 ];
 
 export const LANGCHAIN_USER_ONLY_MODELS = [
-  "o1",
-  "gpt-4o",
-  "gpt-4.5-preview",
-  "claude-3-5-sonnet-latest",
-  "claude-3-7-sonnet-latest",
-  "gemini-2.0-flash-thinking-exp-01-21",
+  // "o1",
+  // "gpt-4o",
+  // "gpt-4.5-preview",
+  // "claude-3-5-sonnet-latest",
+  // "claude-3-7-sonnet-latest",
+  // "gemini-2.0-flash-thinking-exp-01-21",
 ];
 
 // Models which do NOT support the temperature parameter.
